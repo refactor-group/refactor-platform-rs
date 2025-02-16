0d466f41ce9344f71fb57452ea7d9ec451066c4d
Caleb Bourg <calebbourg2@gmail.com>
2025-02-16 10:37:17 -0500

    add ToSchema to Jwt

--- entity/src/jwt.rs
+++ entity/src/jwt.rs
@@ -1,13 +1,16 @@
 use serde::Serialize;
+use utoipa::ToSchema;
 
 /// Represents a JSON Web Token (JWT).
+/// Note: This struct does not have a corresponding entity in the database.
 ///
 /// This struct contains two fields:
 ///
 /// - `token`: A string representing the JWT.
 /// - `sub`: A string representing the subject of the JWT for conveniently accessing
 /// the subject without having to decode the JWT.
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, ToSchema)]
+#[schema(as = entity::jwt::Jwt)] // OpenAPI schema
 pub struct Jwt {
     pub token: String,
     pub sub: String,
