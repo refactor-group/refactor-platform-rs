1113faa9c15d452ff7d9a9f8677611f7cab69d03
Caleb Bourg <calebbourg2@gmail.com>
2025-02-25 15:54:36 -0500

    fixup agreement

--- domain/src/agreement.rs
+++ domain/src/agreement.rs
@@ -1,9 +1,10 @@
 use crate::agreements::Model;
 use crate::error::Error;
-pub use entity_api::agreement::{create, delete_by_id, find_by_id, update};
 use entity_api::IntoQueryFilterMap;
 use entity_api::{agreements, query};
 use sea_orm::DatabaseConnection;
+
+pub use entity_api::agreement::{create, delete_by_id, find_by_id, update};
 
 pub async fn find_by(
     db: &DatabaseConnection,
