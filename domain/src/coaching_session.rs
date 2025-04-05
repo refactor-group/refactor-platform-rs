c1ba826145df7fa951d0d562985f3247c9311deb
Caleb Bourg <calebbourg2@gmail.com>
2025-04-05 09:12:42 -0400

    refactor coaching session code

--- domain/src/coaching_session.rs
+++ domain/src/coaching_session.rs
@@ -1,18 +1,37 @@
 use crate::coaching_sessions::Model;
-use crate::error::{DomainErrorKind, Error, ExternalErrorKind, InternalErrorKind};
-use crate::gateway::tiptap::client as tiptap_client;
+use crate::error::{DomainErrorKind, Error, InternalErrorKind};
+use crate::gateway::tiptap::TiptapDocument;
 use crate::Id;
-use chrono::{DurationRound, TimeDelta};
+use chrono::{DurationRound, NaiveDateTime, TimeDelta};
 use entity_api::{
     coaching_relationship, coaching_session, coaching_sessions, mutate, organization, query,
     query::IntoQueryFilterMap,
 };
 use log::*;
 use sea_orm::{DatabaseConnection, IntoActiveModel};
-use serde_json::json;
 use service::config::Config;
 
 pub use entity_api::coaching_session::{find_by_id, find_by_id_with_coaching_relationship};
+
+#[derive(Debug, Clone)]
+struct SessionDate(NaiveDateTime);
+
+impl SessionDate {
+    fn new(date: NaiveDateTime) -> Result<Self, Error> {
+        let truncated = date.duration_trunc(TimeDelta::minutes(1)).map_err(|err| {
+            warn!("Failed to truncate date_time: {:?}", err);
+            Error {
+                source: Some(Box::new(err)),
+                error_kind: DomainErrorKind::Internal(InternalErrorKind::Other),
+            }
+        })?;
+        Ok(Self(truncated))
+    }
+
+    fn into_inner(self) -> NaiveDateTime {
+        self.0
+    }
+}
 
 pub async fn create(
     db: &DatabaseConnection,
@@ -23,68 +42,20 @@ pub async fn create(
         coaching_relationship::find_by_id(db, coaching_session_model.coaching_relationship_id)
             .await?;
     let organization = organization::find_by_id(db, coaching_relationship.organization_id).await?;
-    // Remove seconds because all coaching_sessions will be scheduled by the minute
-    // TODO: we might consider codifying this in the type system at some point.
-    let date_time = coaching_session_model
-        .date
-        .duration_trunc(TimeDelta::minutes(1))
-        .map_err(|err| {
-            warn!("Failed to truncate date_time: {:?}", err);
-            Error {
-                source: Some(Box::new(err)),
-                error_kind: DomainErrorKind::Internal(InternalErrorKind::Other),
-            }
-        })?;
-    coaching_session_model.date = date_time;
-    let document_name = format!(
-        "{}.{}.{}-v0",
-        organization.slug,
-        coaching_relationship.slug,
-        Id::new_v4()
-    );
+
+    coaching_session_model.date = SessionDate::new(coaching_session_model.date)?.into_inner();
+
+    let document_name = generate_document_name(&organization.slug, &coaching_relationship.slug);
     info!(
         "Attempting to create Tiptap document with name: {}",
         document_name
     );
     coaching_session_model.collab_document_name = Some(document_name.clone());
-    let tiptap_url = config.tiptap_url().ok_or_else(|| {
-        warn!("Failed to get Tiptap URL from config");
-        Error {
-            source: None,
-            error_kind: DomainErrorKind::Internal(InternalErrorKind::Other),
-        }
-    })?;
-    let full_url = format!("{}/api/documents/{}?format=json", tiptap_url, document_name);
-    let client = tiptap_client(config).await?;
 
-    let request = client
-        .post(full_url)
-        .json(&json!({"type": "doc", "content": []}));
-    let response = match request.send().await {
-        Ok(response) => {
-            info!("Tiptap response: {:?}", response);
-            response
-        }
-        Err(e) => {
-            warn!("Failed to send request: {:?}", e);
-            return Err(e.into());
-        }
-    };
+    let tiptap = TiptapDocument::new(config).await?;
+    tiptap.create(&document_name).await?;
 
-    // Tiptap's API will return a 200 for successful creation of a new document
-    // and will return a 409 if the document already exists. We consider both "successful".
-    if response.status().is_success() || response.status().as_u16() == 409 {
-        Ok(coaching_session::create(db, coaching_session_model).await?)
-    } else {
-        warn!(
-            "Failed to create Tiptap document: {}",
-            response.text().await?
-        );
-        Err(Error {
-            source: None,
-            error_kind: DomainErrorKind::External(ExternalErrorKind::Network),
-        })
-    }
+    Ok(coaching_session::create(db, coaching_session_model).await?)
 }
 
 pub async fn find_by(
@@ -127,41 +98,18 @@ pub async fn delete(db: &DatabaseConnection, config: &Config, id: Id) -> Result<
         }
     })?;
 
-    let tiptap_url = config.tiptap_url().ok_or_else(|| {
-        warn!("Failed to get Tiptap URL from config");
-        Error {
-            source: None,
-            error_kind: DomainErrorKind::Internal(InternalErrorKind::Other),
-        }
-    })?;
-    let full_url = format!("{}/api/documents/{}?format=json", tiptap_url, document_name);
-    let client = tiptap_client(config).await?;
+    let tiptap = TiptapDocument::new(config).await?;
+    tiptap.delete(&document_name).await?;
 
-    let request = client.delete(full_url);
-    let response = match request.send().await {
-        Ok(response) => {
-            info!("Tiptap response: {:?}", response);
-            response
-        }
-        Err(e) => {
-            warn!("Failed to send request: {:?}", e);
-            return Err(e.into());
-        }
-    };
+    coaching_session::delete(db, id).await?;
+    Ok(())
+}
 
-    // Tiptap's API will return a 204 for successful deletion of a document
-    // and will return a 404 if the document does not exist.
-    let status = response.status();
-    if status.is_success() {
-        Ok(coaching_session::delete(db, id).await?)
-    } else {
-        warn!(
-            "Failed to delete Tiptap document: {}, with status: {}",
-            document_name, status
-        );
-        Err(Error {
-            source: None,
-            error_kind: DomainErrorKind::External(ExternalErrorKind::Network),
-        })
-    }
+fn generate_document_name(organization_slug: &str, relationship_slug: &str) -> String {
+    format!(
+        "{}.{}.{}-v0",
+        organization_slug,
+        relationship_slug,
+        Id::new_v4()
+    )
 }
