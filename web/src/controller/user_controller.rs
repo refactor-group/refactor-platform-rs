c48b2741b4046d4ca95aad68c84f1d2f902409b9
Caleb Bourg <calebbourg2@gmail.com>
2025-04-10 08:07:50 -0400

    add GET /users/:user_id

--- web/src/controller/user_controller.rs
+++ web/src/controller/user_controller.rs
@@ -12,6 +12,33 @@ use axum::{
 use domain::{user as UserApi, Id};
 use service::config::ApiVersion;
 
+/// GET a User
+///
+#[utoipa::path(
+    get,
+    path = "/users/{user_id}",
+    params(
+        ApiVersion,
+        ("user_id" = Id, Path, description = "User ID", example = "1234567890"),
+    ),
+    responses(
+        (status = 200, description = "Successfully retrieved a User", body = User),
+        (status = 401, description = "Unauthorized"),
+    ),
+    security(
+        ("cookie_auth" = [])
+    )
+)]
+pub async fn read(
+    CompareApiVersion(_v): CompareApiVersion,
+    AuthenticatedUser(_user): AuthenticatedUser,
+    State(app_state): State<AppState>,
+    Path(user_id): Path<Id>,
+) -> Result<impl IntoResponse, Error> {
+    let user = UserApi::find_by_id(app_state.db_conn_ref(), user_id).await?;
+    Ok(Json(ApiResponse::new(StatusCode::OK.into(), user)))
+}
+
 /// UPDATE a User
 /// NOTE: that this is for updating the current user
 #[utoipa::path(
--- web/src/protect/users.rs
+++ web/src/protect/users.rs
@@ -6,7 +6,28 @@ use axum::{
     response::IntoResponse,
 };
 use domain::Id;
-use log::error;
+use log::*;
+
+// checks:
+// - that the `user_id` matches the `authenticated_user.id`
+pub(crate) async fn read(
+    State(_app_state): State<AppState>,
+    AuthenticatedUser(authenticated_user): AuthenticatedUser,
+    Path(user_id): Path<Id>,
+    request: Request,
+    next: Next,
+) -> impl IntoResponse {
+    // check that we are only allowing authenticated users to read themselves (for now)
+    if authenticated_user.id == user_id {
+        next.run(request).await
+    } else {
+        error!(
+            "Unauthorized: user_id {} does not match authenticated_user_id {}",
+            user_id, authenticated_user.id
+        );
+        (StatusCode::UNAUTHORIZED, "Unauthorized").into_response()
+    }
+}
 
 // checks:
 // - that the `user_id` matches the `authenticated_user.id`
@@ -17,6 +38,7 @@ pub(crate) async fn update(
     request: Request,
     next: Next,
 ) -> impl IntoResponse {
+    info!("Authenticated user id: {}", authenticated_user.id);
     // check that we are only allowing authenticated users to update themselves (for now)
     if authenticated_user.id == user_id {
         next.run(request).await
