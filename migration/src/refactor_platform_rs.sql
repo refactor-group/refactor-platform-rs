0c9efe1e6bf05848e44beb45cc0b6eff32bc06df
Caleb Bourg <calebbourg2@gmail.com>
2025-05-09 14:31:06 -0400

    migrate to migrations
    added non-prod migration for adding initial admin user

--- migration/src/refactor_platform_rs.sql
+++ migration/src/refactor_platform_rs.sql
@@ -1,6 +1,6 @@
 -- SQL dump generated using DBML (dbml.dbdiagram.io)
 -- Database: PostgreSQL
--- Generated at: 2025-04-29T11:55:29.465Z
+-- Generated at: 2025-05-09T18:26:53.395Z
 
 
 CREATE TYPE "refactor_platform"."status" AS ENUM (
