f7084af022f462864165af9376d977fa016219d2
Caleb Bourg <calebbourg2@gmail.com>
2025-05-30 14:17:11 -0400

    add initial organization

--- migration/src/m20250509_164646_add_initial_non_prod_user.rs
+++ migration/src/m20250509_164646_add_initial_non_prod_user.rs
@@ -1,5 +1,15 @@
 use chrono::Utc;
-use entity::users::{ActiveModel, Column, Entity};
+use entity::organizations::{
+    ActiveModel as OrganizationActiveModel, Column as OrganizationColumn,
+    Entity as OrganizationEntity,
+};
+use entity::organizations_users::{
+    ActiveModel as OrganizationUsersActiveModel, Column as OrganizationUsersColumn,
+    Entity as OrganizationUsersEntity,
+};
+use entity::users::{
+    ActiveModel as UsersActiveModel, Column as UsersColumn, Entity as UsersEntity,
+};
 use password_auth::generate_hash;
 use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, Set};
 use sea_orm_migration::prelude::*;
@@ -22,8 +32,8 @@ impl MigrationTrait for Migration {
         .unwrap();
 
         match rust_env {
-            RustEnv::Development => insert_initial_admin_user(manager).await,
-            RustEnv::Staging => insert_initial_admin_user(manager).await,
+            RustEnv::Development => insert_initial_admin_user_and_org(manager).await,
+            RustEnv::Staging => insert_initial_admin_user_and_org(manager).await,
             RustEnv::Production => {
                 // We have a different process for initial setup in production
                 Ok(())
@@ -40,8 +50,8 @@ impl MigrationTrait for Migration {
         .unwrap();
 
         match rust_env {
-            RustEnv::Development => delete_initial_admin_user(manager).await,
-            RustEnv::Staging => delete_initial_admin_user(manager).await,
+            RustEnv::Development => delete_initial_admin_user_and_org(manager).await,
+            RustEnv::Staging => delete_initial_admin_user_and_org(manager).await,
             RustEnv::Production => {
                 // We have a different process for initial setup in production
                 Ok(())
@@ -50,11 +60,11 @@ impl MigrationTrait for Migration {
     }
 }
 
-async fn insert_initial_admin_user(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
+async fn insert_initial_admin_user_and_org(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
     let db = manager.get_connection();
     let now = Utc::now();
 
-    ActiveModel {
+    let admin_user = UsersActiveModel {
         email: Set("admin@refactorcoach.com".to_owned()),
         first_name: Set("admin".to_owned()),
         last_name: Set("admin".to_owned()),
@@ -68,21 +78,58 @@ async fn insert_initial_admin_user(manager: &SchemaManager<'_>) -> Result<(), Db
     }
     .save(db)
     .await
-    .map_err(|e| DbErr::from(e))?;
+    .unwrap();
+
+    let org = OrganizationActiveModel {
+        name: Set("Refactor Group".to_owned()),
+        slug: Set("refactor-group".to_owned()),
+        created_at: Set(now.into()),
+        updated_at: Set(now.into()),
+        ..Default::default()
+    }
+    .save(db)
+    .await
+    .unwrap();
+
+    OrganizationUsersActiveModel {
+        organization_id: org.id,
+        user_id: admin_user.id,
+        created_at: Set(now.into()),
+        updated_at: Set(now.into()),
+        ..Default::default()
+    }
+    .save(db)
+    .await
+    .unwrap();
 
     Ok(())
 }
 
-async fn delete_initial_admin_user(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
+async fn delete_initial_admin_user_and_org(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
     let db = manager.get_connection();
 
-    let user = Entity::find()
-        .filter(Column::Email.eq("admin@refactorcoach.com"))
+    let user = UsersEntity::find()
+        .filter(UsersColumn::Email.eq("admin@refactorcoach.com"))
         .one(db)
         .await?
         .unwrap();
 
+    let org = OrganizationEntity::find()
+        .filter(OrganizationColumn::Name.eq("Refactor Group"))
+        .one(db)
+        .await?
+        .unwrap();
+
+    let organizations_users_join = OrganizationUsersEntity::find()
+        .filter(OrganizationUsersColumn::OrganizationId.eq(org.id))
+        .filter(OrganizationUsersColumn::UserId.eq(user.id))
+        .one(db)
+        .await?
+        .unwrap();
+
+    organizations_users_join.delete(db).await?;
     user.delete(db).await?;
+    org.delete(db).await?;
 
     Ok(())
 }
--- migration/src/refactor_platform_rs.sql
+++ migration/src/refactor_platform_rs.sql
@@ -1,6 +1,6 @@
 -- SQL dump generated using DBML (dbml.dbdiagram.io)
 -- Database: PostgreSQL
--- Generated at: 2025-05-09T18:26:53.395Z
+-- Generated at: 2025-05-30T18:15:58.336Z
 
 
 CREATE TYPE "refactor_platform"."status" AS ENUM (
