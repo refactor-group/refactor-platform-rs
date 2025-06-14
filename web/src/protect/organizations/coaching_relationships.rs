d65a090c1f1149c159797c42c7613ea51a3437c2
Caleb Bourg <calebbourg2@gmail.com>
2025-06-14 08:50:24 -0400

    add authorization and validation checks to coaching relationship creation

---
+++ web/src/protect/organizations/coaching_relationships.rs
@@ -0,0 +1,27 @@
+use crate::protect::{Predicate, UserInOrganization, UserIsAdmin};
+use crate::{extractors::authenticated_user::AuthenticatedUser, AppState};
+use axum::{
+    extract::{Path, Request, State},
+    middleware::Next,
+    response::IntoResponse,
+};
+
+use domain::Id;
+
+/// Checks that the authenticated user is associated with the organization specified by `organization_id`
+/// and that the authenticated user is an admin
+/// Intended to be given to axum::middleware::from_fn_with_state in the router
+pub(crate) async fn create(
+    State(app_state): State<AppState>,
+    AuthenticatedUser(authenticated_user): AuthenticatedUser,
+    Path(organization_id): Path<Id>,
+    request: Request,
+    next: Next,
+) -> impl IntoResponse {
+    let checks: Vec<Predicate> = vec![
+        Predicate::new(UserInOrganization, vec![organization_id]),
+        Predicate::new(UserIsAdmin, vec![]),
+    ];
+
+    crate::protect::authorize(&app_state, authenticated_user, request, next, checks).await
+}
